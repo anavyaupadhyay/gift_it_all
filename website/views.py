c00645def2af9b5a428655e59e6188f282723394
Nikhil Singh <nikhilsingh210920@gmail.com>
2023-10-17 15:23:58 +0530

    product list page

--- website/views.py
+++ website/views.py
@@ -19,25 +19,10 @@ class HomePageView(TemplateView):
     """
     template_name = 'website/homepage.html'
 
-
-    # def get_context_data(self, **kwargs):
-    #     context = super(self.__class__, self).get_context_data(**kwargs)
-    #     if cache.get('banner_image_objects'):
-    #         context['banner_image_objects']=cache.get('banner_image_objects')
-    #     else:    
-    #         context['banner_image_objects'] = BannerImages.objects.filter(is_active=True)
-    #         cache.set('banner_image_objects',context['banner_image_objects'])
-    #     if cache.get('featured_product'): 
-    #         context['featured_product']=cache.get('featured_product')
-    #     else:
-    #         context['featured_product'] = Product.objects.filter(is_featured=True).filter(structure='parent')[:4] 
-    #         cache.set('featured_product',context['featured_product'])
-    #     if cache.get('dietitions_and_nutritionists'):
-    #         context['dietitions_and_nutritionists']=cache.get('dietitions_and_nutritionists')
-    #     else:        
-    #         context['dietitions_and_nutritionists'] = DietitionsAndNutritionists.objects.filter(dietitions_and_nutritionists='Dietitions')
-    #         cache.set('dietitions_and_nutritionists',context['dietitions_and_nutritionists'])
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(self.__class__, self).get_context_data(**kwargs)
+        context['products']=Product.objects.all()
+        return context      
 
 class ProductListView(ListView):
     model = Product
@@ -51,14 +36,14 @@ class ProductListView(ListView):
         category_slug = self.kwargs.get('slug', None)
         # 302 redirection in case of incorect url enter
         if category and Category.objects.filter(slug=category, depth=1, is_active=True).exists():
-            return HttpResponseRedirect(reverse('website:product_category', kwargs={'slug': category}))
+            return HttpResponseRedirect(reverse('catalogue:product_category', kwargs={'slug': category}))
 
         if category_slug and not Category.objects.filter(slug=category_slug).exists() and Product.objects.filter(slug=category_slug).exists():
             prod_obj = Product.objects.filter(slug=category_slug).first()
             if prod_obj and prod_obj.structure == "child" and prod_obj.parent:
-                return HttpResponseRedirect(reverse('website:product_detail', kwargs={'slug': prod_obj.parent.slug}))
+                return HttpResponseRedirect(reverse('catalogue:detail', kwargs={'slug': prod_obj.parent.slug}))
             else:
-                return HttpResponseRedirect(reverse('website:product_detail', kwargs={'slug': category_slug}))   
+                return HttpResponseRedirect(reverse('catalogue:detail', kwargs={'slug': category_slug}))   
 
         if category_slug and not Category.objects.filter(slug=category_slug).exists():
             return HttpResponseRedirect(reverse('website:product_list'))    
@@ -66,4 +51,3 @@ class ProductListView(ListView):
         return super().dispatch(request, *args, **kwargs)
     
 
-    
