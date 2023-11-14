ef4c8991967150077e63e16a0bf7382aa5f10b18
Anavya@859118376229 <anavya@sudofire.com>
2023-11-14 21:27:45 +0530

    added whitenoise for staticfiles

--- gift_it_all/settings.py
+++ gift_it_all/settings.py
@@ -233,17 +233,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# STATIC_URL = '/static/'
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'), 
-# )
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'), 
+)
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # OSCAR SETTINGS
 OSCAR_SHOP_NAME = 'GIFT IT ALL'
